719577997ccab604ff4b54bf7a3d04561ab9662e
GlauberRoque <glauberroqueferr@gmail.com>
2021-06-24 23:19:26 -0300

    cpfCommit

---
+++ ValidandoCpf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ValidandoCpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("Digite o seu CPF: ");
+                string CPF = Console.ReadLine();
+            if (CPF.Length > 11 )
+            {
+                Console.WriteLine("Informe um cpf válido!");
+            }else if(CPF.Length < 11){
+                Console.WriteLine("Informe um cpf válido!");
+            }else{
+                CPF = CPF.Insert(9, "-");
+                CPF = CPF.Insert(6, ".");
+                CPF = CPF.Insert(3, ".");
+               
+                Console.WriteLine(CPF);
+            }
+
+        }
+    }
+}
